7375dc0f1c70bc77a55b3b205230e4577c170b42
DrJonYearsley <jon.yearsley@ucd.ie>
2025-03-27 16:40:31 +0000

    update 10km aggregation

--- julia/anomaly_year_average.jl
+++ julia/anomaly_year_average.jl
@@ -43,17 +43,17 @@ run_params = (speciesName="ips_sexdentatus",      # Name of the species
 # =================================================================================
 # Specify directories for import and export of data
 
-if isdir("//home//jon//Desktop//OPRAM")       # Linux workstation
-    paths = (outDir="//home//jon//Desktop//OPRAM//results//",
-        dataDir="//home//jon//DATA//OPRAM//")
+if isdir(joinpath(homedir(),"Desktop//OPRAM"))       # Linux workstation
+    paths = (outDir=joinpath(homedir(),"Desktop//OPRAM//results//"),
+        dataDir=joinpath(homedir(),"DATA//OPRAM//"))
 
-elseif isdir("//users//jon//Google Drive//My Drive//Projects//DAFM_OPRAM//R")   # Mac
-    paths = (outDir="//users//jon//Google Drive//My Drive//Projects//DAFM_OPRAM//results//",
-        dataDir="//users//jon//Google Drive//My Drive//Projects//DAFM_OPRAM//Data//")
+elseif isdir(joinpath(homedir(),"Google Drive//My Drive//Projects//DAFM_OPRAM//R"))   # Mac
+    paths = (outDir=joinpath(homedir(),"Google Drive//My Drive//Projects//DAFM_OPRAM//results//"),
+        dataDir=joinpath(homedir(),"Google Drive//My Drive//Projects//DAFM_OPRAM//Data//"))
 
-elseif isdir("//users//jon//Desktop//OPRAM//")
-    paths = (outDir="//users//jon//Desktop//OPRAM//results//",
-        dataDir="//users//jon//Desktop//OPRAM//")
+elseif isdir(joinpath(homedir(),"Desktop//OPRAM//"))
+    paths = (outDir=joinpath(homedir(),"Desktop//OPRAM//results//"),
+        dataDir=joinpath(homedir(),"Desktop//OPRAM//"))
 end
 
 include("OPRAM_io_functions.jl");
@@ -109,28 +109,122 @@ idx = d.emergeDOY .> 365 * 3.2
 d.nGenerations[idx] .= missing
 d.emergeDOY[idx] .= missing
 
-# COmbine origninal data frame with 30 year average
+# =========================================================
+# =========================================================
+# Combine the multi year median with the original data and calculate anomalies
+
+
+# Combine origninal data frame with 30 year average
 leftjoin!(d, d_agg, on=[:ID, :east, :north, :startMonth])
 # Calculate anomalies
 transform!(d, [:nGenerations, :nGenerations_median] => ((a, b) -> round.(a .- b, digits=3) ) => :nGenerations_anomaly)
 transform!(d, [:emergeDOY, :emergeDOY_median] => ((a, b) -> a .- b ) => :emergeDOY_anomaly)
 
-select!(d, Not(:startMonth))
 
 
-
-# Write out the combined data frame
-fileout1 = joinpath(paths.outDir, run_params.speciesName, "combined_" * run_params.speciesName *
-                "_years_" * string(minimum(run_params.years)) * "_" * string(maximum(run_params.years)) * ".csv")
-CSV.write(fileout1,  d)
+# =========================================================
+# =========================================================
+# Create 10km summary
 
 
-# Write out the average data frame
-fileout2 = joinpath(paths.outDir, run_params.speciesName, "average_" * run_params.speciesName *
-                "_years_" * string(minimum(run_params.years)) * "_" * string(maximum(run_params.years)) * ".csv")
-CSV.write(fileout2,  d_agg)
+  # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Import location data and thin it using thinFactor
+  grid = prepare_data(joinpath([paths.dataDir, run_params.gridFile]), run_params.thinFactor, run_params.country)
+
+  grid.east_hectad = convert.(Int32, floor.(grid.east ./ 1e4) .* 1e4)
+  grid.north_hectad = convert.(Int32, floor.(grid.north ./ 1e4) .* 1e4)
 
 
+  # Checj eastings and northing do identified unique hectads
+tmp = groupby(grid, [:east_hectad, :north_hectad])
+combine(tmp, :hectad => unique)
+
+ # Add bottom left eastings and northings of a hectad into result_1km
+ eastList = sort(unique(grid.east))
+ northList = sort(unique(grid.north))
+ d.east_hectad = convert.(Int32, floor.(d.east ./ 1e4) .* 1e4)
+ d.north_hectad = convert.(Int32, floor.(d.north ./ 1e4) .* 1e4)
+
+
+ # Set missing values to be extremes (e.g. generations are small and emergeDOY is large)
+ idx = ismissing.(d.emergeDOY)
+ d.emergeDOY[idx] .= 365*10
+ d.emergeDOY_median[idx] .= 365*10
+ d.emergeDOY_anomaly[idx] .= 365*10
+
+ d.nGenerations[idx] .= 0
+ d.nGenerations_median[idx] .= 0
+ d.nGenerations_anomaly[idx] .= 0
+
+ # Group data frame by location and then starting Date 
+ df_group = groupby(d, [:east_hectad, :north_hectad, :startDate])
+
+
+ 
+ # Calculate worst case results within each hectad for nGenerations and emergeDOY
+ # for each starting date. 
+ # These worst case scenarios are not affected by locations where no emergence occurred
+ df_10km = combine(df_group,
+   :nGenerations  => (x -> quantile(x, 1.0)) => :nGenerations_max,                  # Max generations per hectad
+   :nGenerations_median  => (x -> quantile(x, 1.0)) => :nGenerations_median_max,
+   :nGenerations_anomaly  => (x -> quantile(x, 1.0)) => :nGenerations_anomaly_max,
+   :emergeDOY => (x -> quantile(x, 0.0)) => :emergeDOY_min,                         # Min emergence DOY
+   :emergeDOY_median => (x -> quantile(x, 0.0)) => :emergeDOY_median_min,
+   :emergeDOY_anomaly => (x -> quantile(x, 0.0)) => :emergeDOY_anomaly_min,
+   :nGenerations  => (x -> sum(x.==0)) => :nMissing  )                              # Count number of no emergence events  
+
+
+ # Add in columns with start and emergence dates
+ df_10km.emergeDate_min = Vector{Union{Missing,Date}}(missing, nrow(df_10km))
+ df_10km.emergeDate_median_min = Vector{Union{Missing,Date}}(missing, nrow(df_10km))
+ idx = df_10km.nMissing .== 0
+ df_10km.emergeDate_min[idx] .= Date.(year.(df_10km.startDate[idx])) .+ Day.(floor.(df_10km.emergeDOY_min[idx]))
+ df_10km.emergeDate_median_min[idx] .= Date.(year.(df_10km.startDate[idx])) .+ Day.(floor.(df_10km.emergeDOY_min[idx]))
+
+
+# Add in hectad unique identifier from the grid file
+out = rightjoin(grid[:,[:east_hectad, :north_hectad, :hectad]],df_10km, on = [:north_hectad, :east_hectad])
+
+
+# scatter(df_nGen2.nMissing, df_nGen2.nGenerations_max,markersize=0.5)
+# scatter(df_nGen2.nMissing.==0, df_nGen2.emergeDOY_min, markersize=0.5)
+# scatter(df_nGen2.nMissing, df_nGen2.startDate, markersize=0.5)
+
+
+# histogram(df_nGen2.nMissing)
+
+# idx = df_nGen2.nMissing.>00
+
+# histogram(df_nGen2.emergeDOY_min[idx], xlimits=(0,1e3))
+# histogram(df_nGen2.emergeDOY_min[Not(idx)], xlimits=(0,1e3))
+
+# histogram(df_nGen2.nGenerations_max[idx], xlimits=(0,2))
+# histogram(df_nGen2.nGenerations_max[Not(idx)], xlimits=(0,2))
+
+# =========================================================
+ # =========================================================
+ # Export the anomaly and multi year average
+
+ # Remove unwanted variables
+ select!(d, Not(:startMonth))
+ 
+ # Write out the combined data frame
+ fileout1 = joinpath(paths.outDir, run_params.speciesName, "combined_" * run_params.speciesName *
+                 "_years_" * string(minimum(run_params.years)) * "_" * string(maximum(run_params.years)) * ".csv")
+ CSV.write(fileout1,  d)
+ 
+ 
+ # Write out the average data frame
+ fileout2 = joinpath(paths.outDir, run_params.speciesName, "average_" * run_params.speciesName *
+                 "_years_" * string(minimum(run_params.years)) * "_" * string(maximum(run_params.years)) * ".csv")
+ CSV.write(fileout2,  d_agg)
+ 
+
+ 
+
+# =========================================================
+# =========================================================
+# Visualise some results
 
 using Plots
 idx = d_agg.startMonth .== unique(d_agg.startMonth)[2]
