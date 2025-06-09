d38cd12bfbfdccae4caf06069236ace1e70f0951
DrJonYearsley <jon.yearsley@ucd.ie>
2025-06-09 09:26:20 +0100

    periodic matrix model

--- julia/periodicMatrixModel.jl
+++ julia/periodicMatrixModel.jl
@@ -22,30 +22,21 @@ using StatsBase
 # Location of results file
 outDir = "//users//jon//Google Drive//My Drive//Projects//DAFM_OPRAM//R"
 
-outDir = "//home//jon//Desktop//OPRAM//results//agrilus"
-outDir = "//users//jon//Google Drive//My Drive//Projects//DAFM_OPRAM//results//agrilus"
+outDir = "//users//jon//Google Drive//My Drive//Projects//DAFM_OPRAM//results//agrilus_anxius/"
 
-importFilePrefix = "result_agrilus"
+importFilePrefix = "agrilus_anxius_IE"
 importYears = 1971:1990
-importYears = 2001:2020
-thin = 10
+importYears = 2015:2020
+thin = 1
 adultLife = 14
-idx_ID = 200
+idx_ID = 200    # ID of the location to use for development matrix
 
 
-# Import results data
-
-# resultsFile = "result_pseudips2019_par_thin10.jld2"
-# result = load(joinpath([outDir,resultsFile]),"single_stored_object")
-
-# result = CSV.read(joinpath([outDir,resultsFile]), DataFrame,
-#                    types = [Int32,Int16,Union{Missing, Int16}])
-
-
-
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Function to create a development matrix for a single location
 function development_matrix!(m, res, adultLifespan)
     # Add one location's development
-    # Rows are emerge days
+    # Rows are emergence days
     # Columns are starting days
     # Sum down one column should equal
     for i in eachindex(res.emergeDOY)
@@ -57,6 +48,7 @@ function development_matrix!(m, res, adultLifespan)
             iEnd = min(365,res.DOY[i+1]-1)
         end
 
+        # Find emergence day of year (i.e. modulo 365)
         adult = mod1.(res.emergeDOY[i] .+ collect(Int16, 0:adultLifespan-1),365)
 
         m[adult, iStart:iEnd] .+= 1
@@ -64,13 +56,50 @@ function development_matrix!(m, res, adultLifespan)
 end
 
 
-# Create matrix columns are doy that development starts
+
+function leslie_matrix(doy, nStages, K, r, s)
+# Function to create a simple Leslie matrix for a single day
+# Arguments:
+#   doy: the day of year that the matrix corresponds to
+#   nStages: the number of stages being tracked by the matrix
+#   K: the development kernel. K[i,j] is the probability that an egg 
+#      laid on day j develops to become reproductively competent on day i
+#   r: the intrinsic growth rate of the population
+#   s: the survival rate of the population
+
+
+    # Create a Leslie matrix for the given day of year
+    L = zeros(Float64, nStages, nStages)
+
+    # Fill in the first row with the reproduction probabilities
+    for i in 1:nStages
+        if i <= size(K, 1)
+            L[1, i] = r * K[doy, mod1(doy-i,size(K,2))]
+        end
+    end
+
+    # Fill in the sub-diagonal with survival probabilities
+    for i in 2:nStages
+        L[i, i-1] = s
+    end
+
+    return L
+end
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Create matrix 
+# Columns are doy that development starts
 # Rows are doy development ends
-# m[i,j] is the probability that an insect starting development on day j emerges on day i
+# mnorm[i,j] is the probability that an adult insect lays eggs on day j (start of development) 
+# and the offspring becomes a competent adult on day i 
+# The sum of mnorm over all i will be 1 
 m = zeros(Int64,365,365);
 
 for y in importYears
-    resultsFile = importFilePrefix * string(y) * "_par_thin" * string(thin) * ".jld2"
+    resultsFile = importFilePrefix * "_" * string(y) * "_" * string(thin) * "km.jld2"
 
     @show resultsFile
     result = load(joinpath([outDir,resultsFile]),"single_stored_object")
@@ -99,34 +128,23 @@ for i in 1:size(mnorm,1)
 end
 
 
+L = Vector{Matrix{Float64}}(undef, 365);
+for i in 1:365
+    L[i] = leslie_matrix(i, 365, mnorm, 4.5, 0.999)
+end
 
-# Display heat map of development traces
-heatmap(log10.(mnorm),    
-    aspect_ratio=:equal,
-    ylimits=(0,365),
-    xlimits=(0, 365))
+a =  reduce(*,reverse(L))
 
-
-ev = eigen(mnorm);
+ev = eigen(a);
 
 emerge_dist = abs.(real(ev.vectors[:,end]));
 
-
-# Display long-term emergence dates
-# findall(abs.(real(ev.vectors[:,end])).>1e-3)
+# Display long-term growth rate
 ev.values[end]
+
+# heatmap(transpose(log10.(a)), yflip=true, aspect_ratio=:equal)
+heatmap(a, yflip=true, aspect_ratio=:equal)
+
 
 # Plot long term distribution of emergence dates
 plot(emerge_dist)
-
-
-# Produce plot of development
-d = transpose(reinterpret(reshape,Int64, findall(!iszero, mnorm)));
-# plot(d[:,2], [d[:,1], d[:,2]], 
-#     aspect_ratio=:equal,
-#     ylimits=(0,365),
-#     xlimits=(0, 365),
-#     color=RGBA{Float64}(0,0,0,0.2))
-
-
-describe(findall(abs.(real(ev.vectors[:,end])).>1e-3))
