da0025ff2c764abcc14066eaccf69173b815e0ef
DrJonYearsley <jon.yearsley@ucd.ie>
2025-08-14 16:47:39 +0100

    finalise aws upload script

--- aws_upload.sh
+++ aws_upload.sh
@@ -1,13 +1,13 @@
 #  bash script to copy files from computer to was s3 bucket
 #
-path="/media/jon/Seagate_5TB/OPRAM_results/data" # the path of the directory where the files and directories that need to be copied are located
-s3Dir="s3://ucd-pest-risk/irish-hectad-map/data" # the s3 bucket path
+#path="/media/jon/Seagate_5TB/OPRAM_results/data" # the path containing the data
+path="/Users/jon/Desktop/gisdata" # the path containing the data
+s3Dir="s3://ucd-pest-risk/irish-hectad-map/gisdata" # the s3 bucket path
 
-for entry in "$path"/base0*; do
+for entry in "$path"/*; do
     name=`echo $entry | sed 's/.*\///'`  # getting the name of the file or directory
-#    if [[ -d  $entry ]]; then  # if it is a directory
-#        aws s3 cp  --recursive --exclude "*" --include "*.csv" "$name" "$s3Dir/$name/"
-#    fi
+    if [[ -d  "$entry" ]]; then  # if it is a directory
+        aws s3 cp  --recursive --exclude "*" --include "*.tif" "$entry" "$s3Dir/$name/"
+    fi
 
-  echo $name
 done
