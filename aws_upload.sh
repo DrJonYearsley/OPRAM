106ff9832f851120a7356c8bdf4205c57a67c968
DrJonYearsley <jon.yearsley@ucd.ie>
2025-08-14 12:39:35 +0100

    update file operations

--- aws_upload.sh
+++ aws_upload.sh
@@ -1,7 +1,7 @@
 #  bash script to copy files from computer to was s3 bucket
 #
 path="/media/jon/Seagate_5TB/OPRAM_results/data" # the path of the directory where the files and directories that need to be copied are located
-s3Dir="ucd-pest-risk/irish-hectad-map/data" # the s3 bucket path
+s3Dir="s3://ucd-pest-risk/irish-hectad-map/data" # the s3 bucket path
 
 for entry in "$path"/base0*; do
     name=`echo $entry | sed 's/.*\///'`  # getting the name of the file or directory
--- modify_filenames.sh
+++ modify_filenames.sh
@@ -1,17 +1,27 @@
 #  bash script to modify filenames
 #
-path="/media/jon/Seagate_5TB/OPRAM_results/data" # the path of the directory where the files and directories that need to be copied are located
+path="/media/jon/Seagate_5TB/OPRAM_results/gisdata" # the path of the directory where the files and directories that need to be copied are located
 
 for d in "$path"/*; do
   echo $d
   
-  # Look at files within the directory
-  for entry in "$d"/*; do
-    newentry=`echo "$entry" | sed -r 's/thesh/thresh/g'`
+  if [ -d "$d" ]; then 
+    # Update dir name
+    newd=`echo "$d" | sed -r 's/thesh/thresh/g'`
 
-    if [ "$entry" != "$newentry" ]; then
-    #  echo $newentry
-      mv "$entry" "$newentry"
+    if [ "$d" != "$newd" ]; then
+      #echo $newd
+      mv "$d" "$newd"
     fi
-  done
+  
+    # Look at files within the directory
+    for entry in "$newd"/*; do
+      newentry=`echo "$entry" | sed -r 's/thesh/thresh/g'`
+
+      if [ "$entry" != "$newentry" ]; then
+        #echo $newentry
+        mv "$entry" "$newentry"
+      fi
+    done
+  fi
 done
