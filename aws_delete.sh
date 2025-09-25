3d3ff392f69c4ac555f16bccbada179ffdb67977
DrJonYearsley <jon.yearsley@ucd.ie>
2025-09-25 08:12:04 +0100

    aws scripts

--- aws_delete.sh
+++ aws_delete.sh
@@ -2,8 +2,14 @@
 #
 
 s3Dir="s3://ucd-pest-risk/irish-hectad-map/data" # the s3 bucket path
-txt="base*"
 
 
-aws s3 ls "$s3Dir"  --recursive  --exclude "$txt"  
+
+#aws s3 ls "$s3Dir"  --recursive
 #aws s3 ls --recursive --exclude "*" --include "average*"  "$s3Dir"
+
+# Always do a dryrun before running a delete command!!
+#aws s3 rm "$s3Dir"  --dryrun --recursive --exclude "*" --include "*average*"
+
+
+#aws s3 rm "$s3Dir"  --recursive --exclude "*" --include "*average*"
--- aws_upload.sh
+++ aws_upload.sh
@@ -1,13 +1,19 @@
 #  bash script to copy files from computer to aws s3 bucket
 #
-path="/media/jon/Seagate_5TB/OPRAM_results/data" # the path containing the data
-#path="/Users/jon/Desktop/gisdata" # the path containing the data
+#path="/media/jon/Seagate_5TB/OPRAM_results/data" # the path containing the data
+path="/Users/jon/OPRAM/results_test" # the path containing the data
 s3Dir="s3://ucd-pest-risk/irish-hectad-map/data" # the s3 bucket path
 
 for entry in "$path"/*; do
   name=`echo $entry | sed 's/.*\///'`  # getting the name of the file or directory
     
   if [[ -d  "$entry" ]]; then  # if it is a directory
+      # Always use --dryrun before commiting to run a command
+#      aws s3 cp  --dryrun --recursive --exclude "*" --include "*.csv" --exclude "average*.csv" "$entry" "$s3Dir/$name/"
       aws s3 cp  --recursive --exclude "*" --include "*.csv" --exclude "average*.csv" "$entry" "$s3Dir/$name/"
   fi
 done
+
+
+# List contents
+# aws s3 ls "$s3Dir"  
